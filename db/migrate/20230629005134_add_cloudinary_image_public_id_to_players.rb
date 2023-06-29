7df7af2dfcd517a3eecd6e1a70e86a749c0beaf3
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2023-06-28 18:04:18 -0700

    more data

--- db/migrate/20230629005134_add_cloudinary_image_public_id_to_players.rb
+++
@@ -1,5 +0,0 @@
-class AddCloudinaryImagePublicIdToPlayers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :players, :cloudinary_image_public_id, :string
-  end
-end
--- db/migrate/20230629005240_remove_image_from_players.rb
+++
@@ -1,5 +0,0 @@
-class RemoveImageFromPlayers < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :players, :image, :string
-  end
-end
