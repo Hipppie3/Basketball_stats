5b2d7dc3ad992c03b3be0b2f5fbb72d2d331d2f3
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2023-07-03 20:15:32 -0700

    test

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,11 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://wondrous-speculoos-749189.netlify.app'
+    origins '*'
     # Specify the domain that is allowed to access your API
 
     resource '/api/*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
+      credentials: false
   end
 end
