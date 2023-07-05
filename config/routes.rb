522b0162421419d496876005c37cf953203ed270
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2023-07-05 15:38:39 -0700

    test

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,7 @@ Rails.application.routes.draw do
     resources :players do
     resources :statistics
     resources :videos
-    collection do
-      match 'options', via: :options
-    end
   end
-
+   get 'cors-test', to: 'test#cors_test'
 
 end
