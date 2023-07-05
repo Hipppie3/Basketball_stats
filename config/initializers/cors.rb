a82195eb74a7cef51910fd6fbc27dc5c0f3e00b6
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2023-07-05 13:53:47 -0700

    test

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource '/api/*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true,
-      optionSuccessStatus:200
+      credentials: true
   end
 end
