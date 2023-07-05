b3153153d4f926e9035a5974cdb241d29e896613
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2023-07-05 11:27:47 -0700

    test

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     # Specify the domain that is allowed to access your API
 
     resource '/api/*',
-      headers: [:post, :get],
+      headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
   end
