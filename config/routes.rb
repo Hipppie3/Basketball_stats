4b67802e076655c9fd9b1ea3e23ed8ab756758b2
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2023-07-08 19:45:37 -0700

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :players do
     resources :statistics
     resources :videos
-    post 'upload_image', to: 'players#upload_image' # Add this line
+    post 'upload_image', on: :member
   end
 
   get 'cors-test', to: 'test#cors_test'
