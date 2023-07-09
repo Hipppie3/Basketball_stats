7f47706e90863ffcb827d2f4189bcd94e7da4c0c
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2023-07-08 19:10:36 -0700

    image upload

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -6,17 +6,27 @@ class PlayersController < ApplicationController
     end
 
     def show
-        player = Player.find(params[:id])
-        statistics = player.statistics
-        videos = player.videos
-        render json: { player: player, statistics: statistics, videos: videos }, status: :ok
-    end
-
-
-    def create
-  player = Player.create!(player_params)
-  render json: player, status: :created
+  player = Player.find(params[:id])
+  statistics = player.statistics
+  videos = player.videos
+  render json: { player: player, statistics: statistics, videos: videos, image_url: player.image.service_url }, status: :ok
 end
+
+
+
+   def create
+  player = Player.new(player_params)
+  if params[:player][:image].present?
+    player.image.attach(params[:player][:image])
+  end
+
+  if player.save
+    render json: player, status: :created
+  else
+    render json: player.errors, status: :unprocessable_entity
+  end
+end
+
 
 
     def update
@@ -39,7 +49,7 @@ end
     private
 
  def player_params
-  params.require(:player).permit(:first_name, :last_name, videos_attributes: [:id, :url, :title], statistics_attributes: [:id, :game_date, :matchup, :w_l, :ppg, :rbg, :apg, :spg, :bpg, :fgm, :fga, :fg_percentage, :two_pm, :two_pa, :three_pm, :three_pa, :oreb, :dreb, :reb, :ast, :stl, :blk, :to, :pts])
+  params.require(:player).permit(:first_name, :last_name, :image, videos_attributes: [:id, :url, :title], statistics_attributes: [:id, :game_date, :matchup, :w_l, :ppg, :rbg, :apg, :spg, :bpg, :fgm, :fga, :fg_percentage, :two_pm, :two_pa, :three_pm, :three_pa, :oreb, :dreb, :reb, :ast, :stl, :blk, :to, :pts])
 end
 
   end
---
+++ db/migrate/20230709015838_add_image_to_players.rb
@@ -0,0 +1,4 @@
+class AddImageToPlayers < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
