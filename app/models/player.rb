acc206b08eb8416f64d19782f579df7446f69612
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2023-07-08 19:22:21 -0700

    image upload

--- app/models/player.rb
+++ app/models/player.rb
@@ -3,4 +3,5 @@ class Player < ApplicationRecord
   has_many :videos, dependent: :destroy
   accepts_nested_attributes_for :statistics
   accepts_nested_attributes_for :videos
+  has_one_attached :image
 end
