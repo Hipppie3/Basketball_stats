0da0a09b2410186f9bc9be92b0f7aea5a189e206
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2023-07-10 16:48:13 -0700

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-  
   resources :players do
     resources :statistics
     resources :videos
     post 'upload_image', on: :member
   end
-   resources :sports do
+
+  resources :sports do
     resources :sport_media_videos, only: [:index, :create, :update, :destroy]
-    get 'sport_media_videos', to: 'sports#sport_media_videos'
   end
 
   get 'cors-test', to: 'test#cors_test'
