f5fd687373cec81777fd3a0bc478782d8eea6654
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2023-07-10 13:52:07 -0700

    test

--- config/application.rb
+++ config/application.rb
@@ -1,16 +1,19 @@
+require_relative "boot"
+
+require "rails/all"
+
+Bundler.require(*Rails.groups)
+
 module BasketballStats
   class Application < Rails::Application
-    config.load_defaults 7.0
+  config.load_defaults 7.0
 
-    # Enable CORS
+  # Enable CORS
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*' # Update with the appropriate origin or '*' to allow all origins
         resource '*', headers: :any, methods: [:get, :post, :options] # Adjust the allowed methods as per your requirements
       end
     end
-
-    # Include flash middleware
-    config.middleware.use ActionDispatch::Flash
   end
 end
