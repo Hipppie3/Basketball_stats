1949cb1e88bdd0dac852586cd5790514dc45879e
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2023-07-09 19:27:40 -0700

    added localhost

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
 
-    origins 'https://wondrous-speculoos-749189.netlify.app'
+    origins 'https://wondrous-speculoos-749189.netlify.app', 'http://localhost:5173/'
     # Specify the domain that is allowed to access your API
 
     resource '/players/*',
