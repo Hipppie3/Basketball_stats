7f5264f15a097e435ddc7c2468077f313355b800
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2023-07-10 14:22:01 -0700

    fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-p1 = Player.create(first_name: "Hiep", last_name: "Truong", sport: basketball)
+p1 = Player.create(first_name: "Hiep", last_name: "Truong", sport: 'basketball')
 
 s1 = Statistic.create(game_date: nil, matchup: "White vs Blue", w_l: "W", ppg: 2.1, rbg: 1.1, apg: 2.3, spg: 3.3, bpg: 0.3, fgm: 1, fga: 3, fg_percentage: 0.33, two_pm: 1, two_pa: 2, three_pm: 0, three_pa: 1, oreb: 2, dreb: 3, reb: 5, ast: 10, stl: 2, blk: 0, to: 4, pts: 2, player_id: p1.id)
 
