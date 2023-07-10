70e39aa84ba80eb896c18bda5c2f1caab19452ca
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2023-07-10 14:18:20 -0700

    all over

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :create]
-  resources :sessions, only: [:new, :create, :destroy]
-  delete '/logout', to: 'sessions#destroy', as: 'logout'
-  get '/me', to: 'users#me'
   
   resources :players do
     resources :statistics
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-p1 = Player.create(first_name: "Hiep", last_name: "Truong")
+p1 = Player.create(first_name: "Hiep", last_name: "Truong", sport: basketball)
 
 s1 = Statistic.create(game_date: nil, matchup: "White vs Blue", w_l: "W", ppg: 2.1, rbg: 1.1, apg: 2.3, spg: 3.3, bpg: 0.3, fgm: 1, fga: 3, fg_percentage: 0.33, two_pm: 1, two_pa: 2, three_pm: 0, three_pa: 1, oreb: 2, dreb: 3, reb: 5, ast: 10, stl: 2, blk: 0, to: 4, pts: 2, player_id: p1.id)
 
@@ -6,21 +6,3 @@ v1 = Video.create(url: "https://www.youtube.com/watch?v=0cFH_T01mis", title: "Ga
 
 # db/seeds.rb
 
-# Create a regular user
-User.create!(
-  name: 'Daniel',
-  email: 'Daniel@yahoo.com',
-  password: '123456',
-  password_confirmation: '123456'
-)
-
-# Create an admin user
-admin_user = User.create!(
-  name: 'Daniel Truong',
-  email: 'Daniel@yahoo.com',
-  password: ENV['ADMIN_PASSWORD'],
-  password_confirmation: ENV['ADMIN_PASSWORD'],
-  admin: true
-)
-
-puts 'Seed data created successfully.'
