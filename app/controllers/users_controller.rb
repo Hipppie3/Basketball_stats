939dcc070d217a3aa6fbc6d3955d084a7673c716
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2023-07-11 14:06:42 -0700

    test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,27 @@
 class UsersController < ApplicationController
-  skip_before_action :authorize, only: [:index, :create, :show]
-  
+  skip_before_action :authorize, only: [:index, :create, :me]
 def index
   users = User.all
   render json: users
 end
-
-   def create
-      user = User.create!(user_params)
+  def create
+    user = User.new(user_params)
+    if user.save
       session[:user_id] = user.id
       render json: user, status: :created
-   end
+    else
+      render json: { error: user.errors.full_messages.join(', ') }, status: :unprocessable_entity
+    end
+  end
 
- def show
-      user = User.find(session[:user_id])
+  def me
+    user = User.find_by(id: session[:user_id])
+    if user
       render json: user
-   end
+    else
+      render json: { error: 'User not found' }, status: :not_found
+    end
+  end
 
   # Other controller actions...
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  
-  resources :users, only: [:index, :create]
-
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-get '/me', to: 'users#show'
-  
+
+  resources :users, only: [:index, :create, :update, :show] do
+    collection do
+      get :index
+    end
+    get '/me', to: 'users#me'
+  end
 
   resources :players do
     resources :statistics
