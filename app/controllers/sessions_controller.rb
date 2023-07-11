9d8936e12259ee072d75e96ceec922a8525cca6a
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2023-07-11 13:53:31 -0700

    test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,8 @@
 class SessionsController < ApplicationController
-  skip_before_action :authorize, only: [:create, :show]
+  skip_before_action :authorize, only: [:create]
 
   def create
-    user = User.find_by!(username: params[:username])
+    user = User.find_by(username: params[:username])
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
       render json: user, status: :created
@@ -11,9 +11,12 @@ class SessionsController < ApplicationController
     end
   end
 
- def destroy
-  session.delete :user_id 
-  head :no_content
- end
-  
+  def destroy
+    if session[:user_id]
+      session.delete(:user_id)
+      head :no_content
+    else
+      render json: { error: 'Not logged in' }, status: :unauthorized
+    end
+  end
 end
