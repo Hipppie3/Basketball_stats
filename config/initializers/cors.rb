691dd44983dbd0fe1b3341e19ae12640b9d0c718
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2023-07-10 18:50:59 -0700

    test

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -16,7 +16,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       headers: :any,
       methods: [:get, :post, :delete, :options, :head], # Add any other methods needed for the '/sessions' path
       credentials: true
-      resource '/user',
+      resource '/users/*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
