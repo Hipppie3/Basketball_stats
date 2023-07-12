01a703ed680f61eac1c08c06ca537afadd9c19de
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2023-07-11 19:38:40 -0700

    test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,16 @@
 class SessionsController < ApplicationController
   skip_before_action :authorize, only: [:create]
 
- def create
+def create
   user = User.find_by(username: params[:username])
   if user&.authenticate(params[:password])
     session[:user_id] = user.id
-    render json: user, status: :created
+    render json: { success: true }, status: :ok
   else
     render json: { error: 'Incorrect username or password, please try again' }, status: :unauthorized
   end
 end
+
 
 
  def destroy
