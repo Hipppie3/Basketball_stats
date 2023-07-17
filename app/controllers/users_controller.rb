424779213237dca5bbb3da71dc9a27f7ed54f3a2
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2023-07-17 14:43:46 -0700

    test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,39 @@
 class UsersController < ApplicationController
-  skip_before_action :authorize, only: [:create]
-
-
-  def create
-      user = User.create!(user_params)
-      session[:user_id] = user.id
-      render json: user, status: :created
-   end
-
-
-  def show
-      user = User.find(session[:user_id])
-      render json: user
-  end
-
-    
+  skip_before_action :authorize, only: [:index, :create]
+  
 def index
   users = User.all
   render json: users
 end
 
-  # Other controller actions...
+  def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      render json: user, status: :created
+    else
+      render json: { error: user.errors.full_messages.join(', ') }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+      user = User.find(session[:user_id])
+      render json: user
+   end
+  
+   def me
+    user = User.find_by(id: session[:user_id])
+    if user
+      render json: user
+    else
+      render json: { error: 'User not found' }, status: :not_found
+    end
+  end
 
   private
 
   def user_params
-    params..permit(:id, :username, :name, :password, :password_confirmation, :email)
+    params.require(:user).permit(:username, :name, :password, :password_confirmation, :email)
   end
 end
+  
