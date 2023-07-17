1d83c1358778263085419811207f1f2eb74236df
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2023-07-17 12:17:25 -0700

    test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,14 +7,10 @@ def index
 end
 
   def create
-    user = User.new(user_params)
-    if user.save
+      user = User.create!(user_params)
       session[:user_id] = user.id
       render json: user, status: :created
-    else
-      render json: { error: user.errors.full_messages.join(', ') }, status: :unprocessable_entity
-    end
-  end
+   end
 
 
   def me
