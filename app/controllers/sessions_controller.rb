f615f8c15174bc03e525bad0ec1a8565e870c96c
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2023-07-17 14:54:54 -0700

    test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,8 @@
 class SessionsController < ApplicationController
-  skip_before_action :authorize, only: [:create, :show]
+  skip_before_action :authorize, only: [:create, :show, :me]
 
 def create
-  user = User.find_by(username: params[:username])
+  user = User.find_by!(username: params[:username])
   if user&.authenticate(params[:password])
     session[:user_id] = user.id
     render json: user, status: :ok
