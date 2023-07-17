081b91eeced96ee6edc74c765af29a3be2547281
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2023-07-17 16:40:20 -0700

    test

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
     resource '/players/*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      methods: [:get, :post, :show, :put, :patch, :delete, :options, :head],
       credentials: true
 
     resource '/login',
