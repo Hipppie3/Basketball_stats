5eb8e5b0318afe71a017592910e8142e03ef2da5
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2023-07-20 19:44:22 -0700

    added sport_id

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -69,6 +69,6 @@ class PlayersController < ApplicationController
   private
 
   def player_params
-    params.require(:player).permit(:first_name, :last_name, :image, videos_attributes: [:id, :url, :title], statistics_attributes: [:id, :game_date, :matchup, :w_l, :ppg, :rbg, :apg, :spg, :bpg, :fgm, :fga, :fg_percentage, :two_pm, :two_pa, :three_pm, :three_pa, :oreb, :dreb, :reb, :ast, :stl, :blk, :to, :pts])
+    params.require(:player).permit(:first_name, :last_name, :image, :sport_id, videos_attributes: [:id, :url, :title], statistics_attributes: [:id, :game_date, :matchup, :w_l, :ppg, :rbg, :apg, :spg, :bpg, :fgm, :fga, :fg_percentage, :two_pm, :two_pa, :three_pm, :three_pa, :oreb, :dreb, :reb, :ast, :stl, :blk, :to, :pts])
   end
 end
