f9ebf94be588b182926b4a9906953c19a2e3f915
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2023-07-22 17:46:14 -0700

    test

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,10 +1,12 @@
 class TeamsController < ApplicationController
   def index
-    @teams = Team.all
+    teams = Team.all
+    render json: teams
   end
 
   def show
-    @team = Team.find(params[:id])
+    team = Team.find(params[:id])
+    render json: team
   end
 
   def new
