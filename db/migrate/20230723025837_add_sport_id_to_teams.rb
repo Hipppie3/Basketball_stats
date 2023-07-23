b931265b43c8a4b9e4d8daa2942987f921cbe01a
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2023-07-22 20:02:55 -0700

    test

--- db/migrate/20230723025837_add_sport_id_to_teams.rb
+++ db/migrate/20230723025837_add_sport_id_to_teams.rb
@@ -1,5 +1,5 @@
 class AddSportIdToTeams < ActiveRecord::Migration[7.0]
   def change
-    add_reference :teams, :sport, null: false, foreign_key: true
+    add_reference :teams, :sport, foreign_key: true, null: true
   end
 end
