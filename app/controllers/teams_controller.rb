ce5e20231514d32ed4c0ed62ed851f08283ce534
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2023-07-22 19:48:23 -0700

    test

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,13 +1,30 @@
 class TeamsController < ApplicationController
+  
   def index
-    teams = Team.all
-    render json: teams
-  end
+    teams = Team.all.includes(players: [:statistics, :videos])
+
+    teams_data = teams.map do |team|
+      {
+        id: team.id,
+        name: team.name,
+        players: players_data_with_statistics(team.players)
+      }
+    end
+
 
   def show
     team = Team.find(params[:id])
-    render json: team.to_json(include: :players)
+    players = team.players.includes(:statistics, :videos)
+
+    team_data = {
+      id: team.id,
+      name: team.name,
+      players: players_data_with_statistics(players)
+    }
+
+    render json: team_data, status: :ok
   end
+
 
   def new
     @team = Team.new
@@ -30,6 +47,20 @@ class TeamsController < ApplicationController
 
   private
 
+def players_data_with_statistics(players)
+    players.map do |player|
+      {
+        id: player.id,
+        first_name: player.first_name,
+        last_name: player.last_name,
+        sport_id: player.sport_id,
+        team_id: player.team_id,
+        image_url: player.image.attached? ? url_for(player.image) : nil,
+        statistics: player.statistics,
+        videos: player.videos
+      }
+    end
+  end
   def team_params
     params.require(:team).permit(:name)
   end
