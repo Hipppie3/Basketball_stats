76ce13dd599f35fa3da5b1cabd1f48c83a926fe5
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2023-07-25 10:27:05 -0700

    test

--- app/models/player.rb
+++ app/models/player.rb
@@ -6,6 +6,7 @@ class Player < ApplicationRecord
   has_one_attached :image
   belongs_to :sport, optional: true
   belongs_to :team, optional: true
+  has_many :game_players
   has_many :games, through: :game_players
 end
  
