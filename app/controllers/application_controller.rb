6050e67d07dc0f08525cdba0e72f3f39b05e469f
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2023-08-02 14:33:32 -0700

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,13 @@ class ApplicationController < ActionController::API
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
     rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
-  
+   before_action :authorize
+
     private
+
+      def authorize
+      return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
+    end
 
     def render_not_found_response(exception)
     render json: { error: "#{exception.model} not found"}, status: :not_found
