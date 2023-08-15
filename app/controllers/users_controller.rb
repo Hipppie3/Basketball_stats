34bf349a2dad1324639cc2d5531ef6525eba5bb5
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2023-08-14 18:35:24 -0700

    test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,10 +26,26 @@ end
     end
   end
 
+   def update
+    user = User.find_by(id: session[:user_id])
+    if user
+      if user.update(user_password_params)
+        render json: user, status: :ok
+      else
+        render json: { error: user.errors.full_messages.join(', ') }, status: :unprocessable_entity
+      end
+    else
+      render json: { error: 'User not found' }, status: :not_found
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:username, :name, :password, :password_confirmation, :email)
   end
+
+  def user_password_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
 end
-  
